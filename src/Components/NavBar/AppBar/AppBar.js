d2b5739f5b28e725edd6f7fa2f485f048db5d2ab
Vhugo2001 <vitorhugolb2001@gmail.com>
2020-10-11 18:07:26 -0300

    Auto stash before merge of "main" and "origin/main"

--- src/Components/NavBar/AppBar/AppBar.js
+++ src/Components/NavBar/AppBar/AppBar.js
@@ -1,82 +1,112 @@
 import React, { useState } from "react";
 import './AppBar.css'
 import 'bootstrap/dist/css/bootstrap.css'
-import { Nav, Collapse, Navbar, Image, Row, Col, Card, Container } from 'react-bootstrap'
+import { Nav, Collapse, Navbar, Image, Row, Col, Dropdown, Container } from 'react-bootstrap'
 import Routes from '../../../routes/routes'
+import { Link } from 'react-router-dom'
 
 const AppBar = (props) => {
-    const [openPBL, setOpenPBL] = useState(false);
-    const [openAluno, setOpenAluno] = useState(false);
-    const [openEmpresa, setOpenEmpresa] = useState(false);
+    // const [openPBL, setOpenPBL] = useState(false);
+    // const [openAluno, setOpenAluno] = useState(false);
+    // const [openEmpresa, setOpenEmpresa] = useState(false);
 
-    // <Navbar.Toggle aria-controls="basic-navbar-nav" />
-    //                 <Navbar.Collapse id="basic-navbar-nav">
-    //                 </Navbar.Collapse>
     return (
         <Row className='BarMedidas RowPrincipal'>
-            <Col xs={4} sm={4} md={4} lg={2} xl={2} className='BarMedidas'>
-                <Nav className="flex-column sidenavigation" >
-                    <Nav.Item className='logoUffTopo'>
+            <Col xs={5} sm={4} md={3} lg={2} xl={2} className='BarMedidas '>
+                <Navbar className="flex-column sidenavigation" expand="lg" >
+                    <Navbar.Brand >
                         <Image className='logoUff' src={require('../../Images/Logo_UFF_2.png')} />
-                    </Nav.Item>
+                    </Navbar.Brand>
+                    {/* <Navbar.Toggle aria-controls="basic-navbar-nav" />
+                    <Navbar.Collapse id="basic-navbar-nav" > */}
 
-                    {/* <Nav.Item className='Links'>
-                        <Nav.Link className='DropDownSIdeBar' onClick={() => setOpenPBL(!openPBL)}
-                            aria-controls="example-collapse-text"
-                            aria-expanded={openPBL}>Pbl</Nav.Link>
-                        <Collapse in={openPBL}>
-                            <Nav.Item id="example-collapse-text" >
-                                <Nav.Link className='DropDownSIdeBar' href="/">Cadastrar PBL</Nav.Link>
-                                <Nav.Link className='DropDownSIdeBar' href="/">Consultar PBL</Nav.Link>
-                            </Nav.Item>
-                        </Collapse>
+                    <Nav className="flex-column SideNav mb-auto" >
+                        <Nav.Item className='MenusLaterais'>PBL</Nav.Item>
+                        <Nav.Item className='SubMenusLaterais'>
+                            <Image className='iconesSubMenu' src={require('../../Images/paperPlane.png')} />
+                            <Link to='/teste' className='OpcoesLaterais'>Cadastrar PBL</Link>
+                        </Nav.Item>
+                        <Nav.Item className='SubMenusLaterais'>
+                            <Image className='iconesSubMenu' src={require('../../Images/paperPlane.png')} />
+                            <Link to='/consult' className='OpcoesLaterais'>Consultar PBL</Link>
+                        </Nav.Item>
 
-                        <Nav.Link className='DropDownSIdeBar' onClick={() => setOpenAluno(!openAluno)}
-                            aria-controls="example-collapse-text"
-                            aria-expanded={openAluno}>Aluno</Nav.Link>
-                        <Collapse in={openAluno}>
-                            <Nav.Item id="example-collapse-text">
-                                <Nav.Link className='DropDownSIdeBar' href="/">Cadastrar Aluno</Nav.Link>
-                                <Nav.Link className='DropDownSIdeBar' href="/">Consultar Aluno</Nav.Link>
-                            </Nav.Item>
-                        </Collapse>
+                        <Nav.Item className='MenusLaterais'>Aluno</Nav.Item>
+                        <Nav.Item className='SubMenusLaterais'>
+                            <Image className='iconesSubMenu' src={require('../../Images/paperPlane.png')} />
+                            <Link to='/teste' className='OpcoesLaterais'>Cadastrar Aluno</Link>
+                        </Nav.Item>
+                        <Nav.Item className='SubMenusLaterais'>
+                            <Image className='iconesSubMenu' src={require('../../Images/paperPlane.png')} />
+                            <Link to='/consult' className='OpcoesLaterais'>Consultar Aluno</Link>
+                        </Nav.Item>
 
-                        <Nav.Link className='DropDownSIdeBar' onClick={() => setOpenEmpresa(!openEmpresa)}
-                            aria-controls="example-collapse-text"
-                            aria-expanded={openEmpresa}>Empresa</Nav.Link>
-                        <Collapse in={openEmpresa}>
-                            <Nav.Item id="example-collapse-text">
-                                <Nav.Link className='DropDownSIdeBar' href="/">Cadastrar Empresa</Nav.Link>
-                                <Nav.Link className='DropDownSIdeBar' href="/">Consultar Empresa</Nav.Link>
-                            </Nav.Item>
-                        </Collapse>
-                    </Nav.Item> */}
-                    <Nav.Link className='DropDownSIdeBar' href="/login">Login</Nav.Link>
-                    <Nav.Link className='DropDownSIdeBar' href="/consult">Consultar PBL</Nav.Link>
-                    <Nav.Link className='DropDownSIdeBar' href="/Cadastro">Cadastrar</Nav.Link>
-                    <Nav.Link className='DropDownSIdeBar' href="/">O que é PBL</Nav.Link>
-                </Nav>
+                        <Nav.Item className='MenusLaterais'>Teste</Nav.Item>
+                        <Nav.Item className='SubMenusLaterais'>
+                            <Image className='iconesSubMenu' src={require('../../Images/paperPlane.png')} />
+                            <Link to='/cadastro' className='OpcoesLaterais'>Cadastrar Empresa</Link>
+                        </Nav.Item>
+                        <Nav.Item className='SubMenusLaterais'>
+                            <Image className='iconesSubMenu' src={require('../../Images/paperPlane.png')} />
+                            <Link to='/' className='OpcoesLaterais'>Consultar Empresa</Link>
+                        </Nav.Item>
+                    </Nav>
+                    {/* </Navbar.Collapse> */}
+
+                </Navbar>
             </Col>
 
-            <Col xs={8} sm={8} md={8} lg={10} xl={10} className='BarMedidas'>
-                <Navbar className='main-navbar' expand="sm" variant="dark">
+            <Col xs={7} sm={8} md={9} lg={10} xl={10} className='BarMedidas'>
+                <Navbar className='topbar' expand="sm" variant="dark">
                     <Navbar.Brand>
                         <Nav.Item className='textos-navs'>Menu</Nav.Item>
                     </Navbar.Brand>
                     <Nav className="mr-auto">
                     </Nav>
-                    <Image className='icones' src={require('../../Images/pesquisar.png')} />
-                    <Image className='icones' src={require('../../Images/calendar.png')} />
-                    <Image className='icones' src={require('../../Images/sino.png')} />
-                    <Nav.Link className='textos-navs'>Pessoa Logada</Nav.Link>
+
+                    <Dropdown>
+                        <div class="dropdown-toggle iconesDrop" id="dropdownMenuButton" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                            <Image className='icones' src={require('../../Images/calendar.png')} />
+                        </div>
+                        <div class="dropdown-menu" aria-labelledby="dropdownMenuButton">
+                            <Link to='/teste' className='OpcoesLaterais dropdown-item'>Teste</Link>
+                            <Link to='/login' className='OpcoesLaterais dropdown-item'>Login</Link>
+                            <Link to='/' className='OpcoesLaterais dropdown-item'>Home</Link>
+                        </div>
+                    </Dropdown>
+
+                    <Dropdown>
+                        <div class="dropdown-toggle iconesDrop" id="dropdownMenuButton" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                            <Image className='icones' src={require('../../Images/sino.png')} />
+                        </div>
+                        <div class="dropdown-menu" aria-labelledby="dropdownMenuButton">
+                            <Link to='/teste' className='OpcoesLaterais dropdown-item'>Teste</Link>
+                            <Link to='/login' className='OpcoesLaterais dropdown-item'>Login</Link>
+                            <Link to='/' className='OpcoesLaterais dropdown-item'>Home</Link>
+                        </div>
+                    </Dropdown>
+
+                    <Dropdown>
+                        <div class="dropdown-toggle iconesDrop" id="dropdownMenuButton" data-toggle="dropdown"
+                            style={{ justifyContent: 'flex-start' }} aria-haspopup="true" aria-expanded="false">
+                            <Nav.Link className='textos-navs'>Pessoa Logada</Nav.Link>
+                        </div>
+                        <div class="dropdown-menu" aria-labelledby="dropdownMenuButton">
+                            <Link className='OpcoesLaterais dropdown-item'>Editar Perfil</Link>
+                            <Link onClick={() => alert('Deslogado')} className='OpcoesLaterais dropdown-item'>Sair</Link>
+                        </div>
+                    </Dropdown>
+
+
                     <Image className='iconeLogin' src={require('../../Images/loginIcon.png')} />
                 </Navbar>
 
-                <Container className='CardPrincipal'>
+                <Container>
                     <div className='CardTitulo'>Iniciar PBL</div>
-                    <Routes />
+                    <div className='CardPrincipal' >
+                        <Routes />
+                    </div>
                 </Container>
-
 
             </Col>
         </Row >
--- src/pages/teste/teste.js
+++ src/pages/teste/teste.js
@@ -22,64 +22,63 @@ const Teste = () => {
   }, []);
 
   useEffect(() => {
-   console.log(temaSelecionado)
+    console.log(temaSelecionado)
   }, [temaSelecionado])
 
   return (
     //Remover a div pai e atribur o padding 30px no componente Home!!!!!
-    <div style={{ padding: "30px" }}>
-      <Card>
-        <Card.Form>
-          <Card.Form.Group style={{ flex: 5 }}>
-            <Card.Form.Title>Tema PBL</Card.Form.Title>
-            <DropDownList
-              lista={lista}
-              onSelect={setTemaSelecionado}
-            ></DropDownList>
-          </Card.Form.Group>
 
-          <Card.Form.Group>
-            <Card.Form.Title>Data Inicio</Card.Form.Title>
-            <Card.Form.InputText />
-          </Card.Form.Group>
+    <Card>
+      <Card.Form>
+        <Card.Form.Group style={{ flex: 5 }}>
+          <Card.Form.Title>Tema PBL</Card.Form.Title>
+          <DropDownList
+            lista={lista}
+            onSelect={setTemaSelecionado}
+          ></DropDownList>
+        </Card.Form.Group>
 
-          <Card.Form.Group>
-            <Card.Form.Title>Data Conclusão</Card.Form.Title>
-            <Card.Form.InputText />
-          </Card.Form.Group>
+        <Card.Form.Group>
+          <Card.Form.Title>Data Inicio</Card.Form.Title>
+          <Card.Form.InputText />
+        </Card.Form.Group>
 
-          <Card.Form.BreakRow />
+        <Card.Form.Group>
+          <Card.Form.Title>Data Conclusão</Card.Form.Title>
+          <Card.Form.InputText />
+        </Card.Form.Group>
 
-          <Card.Form.Group style={{ flex: 5 }}>
-            <Card.Form.Title>Alunos</Card.Form.Title>
-            <Card.Form.InputText />
-          </Card.Form.Group>
+        <Card.Form.BreakRow />
 
-          <Card.Form.Group>
-            <Card.Form.Title>Empresa</Card.Form.Title>
-            <Card.Form.InputText />
-          </Card.Form.Group>
+        <Card.Form.Group style={{ flex: 5 }}>
+          <Card.Form.Title>Alunos</Card.Form.Title>
+          <Card.Form.InputText />
+        </Card.Form.Group>
 
-          <Card.Form.BreakRow />
+        <Card.Form.Group>
+          <Card.Form.Title>Empresa</Card.Form.Title>
+          <Card.Form.InputText />
+        </Card.Form.Group>
 
-          <Card.Form.Group>
-            <Card.Form.Title>Titulo PBL</Card.Form.Title>
-            <Card.Form.InputText />
-          </Card.Form.Group>
+        <Card.Form.BreakRow />
 
-          <Card.Form.BreakRow />
+        <Card.Form.Group>
+          <Card.Form.Title>Titulo PBL</Card.Form.Title>
+          <Card.Form.InputText />
+        </Card.Form.Group>
 
-          <Card.Form.Group>
-            <Card.Form.Title>Situação problema</Card.Form.Title>
-            <Card.Form.InputTextArea />
-          </Card.Form.Group>
+        <Card.Form.BreakRow />
 
-          <Card.Form.GroupButton>
-            <Card.Form.Submit>Salvar</Card.Form.Submit>
-          </Card.Form.GroupButton>
-        </Card.Form>
-      </Card>
-    </div>
+        <Card.Form.Group>
+          <Card.Form.Title>Situação problema</Card.Form.Title>
+          <Card.Form.InputTextArea />
+        </Card.Form.Group>
+
+        <Card.Form.GroupButton>
+          <Card.Form.Submit>Salvar</Card.Form.Submit>
+        </Card.Form.GroupButton>
+      </Card.Form>
+    </Card>
   );
 };
 export default Teste;
