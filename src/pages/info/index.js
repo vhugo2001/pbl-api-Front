059e549ace6d029450b87f8c59aa305fea7c9cb2
Diego <diego.canedo86@gmail.com>
2020-10-14 15:52:40 -0300

    Apresentacao Iniciar Pbl

--- src/pages/info/index.js
+++ src/pages/info/index.js
@@ -5,36 +5,7 @@ import "bootstrap/dist/css/bootstrap.min.css";
 function Info() {
   return (
     <>
-      <div>
-        <text>Info</text>
-      </div>
-      <div style={{ marginTop: "60px" }}>
-        <text>Info</text>
-      </div>
-      <div style={{ marginTop: "60px" }}>
-        <text>Info</text>
-      </div>
-      <div style={{ marginTop: "60px" }}>
-        <text>Info</text>
-      </div>
-      <div style={{ marginTop: "60px" }}>
-        <text>Info</text>
-      </div>
-      <div style={{ marginTop: "60px" }}>
-        <text>Info</text>
-      </div>
-      <div style={{ marginTop: "60px" }}>
-        <text>Info</text>
-      </div>
-      <div style={{ marginTop: "60px" }}>
-        <text>Info</text>
-      </div>
-      <div style={{ marginTop: "60px" }}>
-        <text>Info</text>
-      </div>
-      <div style={{ marginTop: "60px" }}>
-        <text>Login</text>
-      </div>
+      
     </>
   );
 }
