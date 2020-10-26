48ada0fa23d80d4b34ac7f80dbbcc470f847fb58
Diego <diego.canedo86@gmail.com>
2020-10-26 07:11:21 -0300

    Testando aluno logado no componente Perfil

---
+++ src/pages/professor/dashboard/dashboard.js
@@ -0,0 +1,17 @@
+import React from "react";
+import { Container, CardContainer, Breakrow } from "./styles";
+
+const dashboard = () => {
+  return (
+    <Container>
+      <CardContainer>aadasdasdasdaa</CardContainer>
+      <CardContainer><p></p><p></p><p></p><p></p><p></p><p></p><p></p><p></p><p></p><p></p><p></p><p></p><p></p><p></p><p></p><p></p></CardContainer>
+<Breakrow></Breakrow>
+<CardContainer><p></p><p></p><p></p><p></p><p></p><p></p><p></p><p></p><p></p><p></p><p></p><p></p><p></p><p></p><p></p><p></p></CardContainer>
+
+      <CardContainer>aaasdadsdaa</CardContainer>
+    </Container>
+  );
+};
+
+export default dashboard;
