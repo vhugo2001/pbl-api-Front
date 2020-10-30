1e3d2f20ece78946588111d287251a13a77d031c
Diego <diego.canedo86@gmail.com>
2020-10-30 10:28:38 -0300

    Form de Disciplina

---
+++ src/pages/disciplina/manter/Index.js
@@ -0,0 +1,31 @@
+import React, { useEffect, useState } from "react";
+import { Row, Col } from "react-bootstrap";
+import Form from "../cadastrar/Index";
+import Listar from "../listar/index";
+
+const Index = () => {
+  const [selectedDisciplina, setSelectedDisciplina] = useState("");
+
+  useEffect(() => {
+
+  }, [selectedDisciplina]);
+
+  return (
+    <>
+      <div className="title-container">
+        <h1>Manter disciplina</h1>
+      </div>
+
+      <Row>
+        <Col xl={4} lg={4} style={{ paddingLeft: "6px", paddingRight: "6px" }}>
+          <Form  selected={selectedDisciplina}/>
+        </Col>
+        <Col xl={8} lg={8} style={{ paddingLeft: "6px", paddingRight: "6px" }}>
+          <Listar selected={setSelectedDisciplina} />
+        </Col>
+      </Row>
+    </>
+  );
+};
+
+export default Index;
