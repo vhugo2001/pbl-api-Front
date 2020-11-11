e85972dce6072258e1769bf4acc5e35a3d7ba6b4
Vhugo2001 <vitorhugolb2001@gmail.com>
2020-11-11 13:33:57 -0300

    Ajustes do npm

--- src/routes/routes.js
+++ src/routes/routes.js
@@ -2,7 +2,7 @@ import React from "react";
 import { Switch } from "react-router-dom";
 import PrivateRoute from "../Components/PrivatRouter/Router";
 
-import  {Role} from '../helpers/role';
+import { Role } from '../helpers/role';
 import listagemAtividade from "../pages/listagemAtividades/index";
 import ConsultarAlunos from "../pages/consultarAlunos";
 import PerfilUsuario from "../pages/perfilUsuario/FormPerfil";
@@ -20,22 +20,20 @@ import Calendario from "../pages/calendario/index"
 const Routes = () => (
   <Switch>
     <PrivateRoute path="/admin/pbl" roles={[Role.Professor]} component={DashBoardProfessor} />
-    <PrivateRoute path="/admin/dashboard" roles={[Role.Professor]}  component={Dashboard} />
-    <PrivateRoute path="/admin/iniciar-pbl" roles={[Role.Professor]}  component={IniciarPBL} />
-    <PrivateRoute path="/admin/alunos"  roles={[Role.Professor]} component={ConsultarAlunos} />
+    <PrivateRoute path="/admin/dashboard" roles={[Role.Professor]} component={Dashboard} />
+    <PrivateRoute path="/admin/iniciar-pbl" roles={[Role.Professor]} component={IniciarPBL} />
+    <PrivateRoute path="/admin/alunos" roles={[Role.Professor]} component={ConsultarAlunos} />
     <PrivateRoute path="/admin/perfil-usuario" component={PerfilUsuario} />
     {/* <Route path="/cadastro" component={Cadastro} /> */}
-    <PrivateRoute path="/admin/listagemAtividades" roles={[Role.Professor]} component={listagemAtividade}/>
-    <PrivateRoute path="/admin/cadastro" roles={[Role.Professor]}  component={Cadastro} />
-    <PrivateRoute path="/admin/manter-disciplina" roles={[Role.Professor]}  component={ManterDisciplina} />
-    <PrivateRoute path="/admin/manter-tema" roles={[Role.Professor]}  component={ManterTema} />
+    <PrivateRoute path="/admin/listagemAtividades" roles={[Role.Professor]} component={listagemAtividade} />
+    <PrivateRoute path="/admin/cadastro" roles={[Role.Professor]} component={Cadastro} />
+    <PrivateRoute path="/admin/manter-disciplina" roles={[Role.Professor]} component={ManterDisciplina} />
+    <PrivateRoute path="/admin/manter-tema" roles={[Role.Professor]} component={ManterTema} />
     <PrivateRoute path="/admin/cadastro" component={Cadastro} />
-    <PrivateRoute path="/admin/manter-disciplina" component={ManterDisciplina} />
-    <PrivateRoute path="/admin/manter-tema" component={ManterTema} />
-    <PrivateRoute path="/admin/manter-problema" component={ManterProblema} />
-    <PrivateRoute path="/admin/manter-contato" component={ManterContato} />
-    <PrivateRoute path="/admin/manter-tarefa" component={ListagemTarefas} />
-    <PrivateRoute path="/admin/calendario" component={Calendario} />
+    <PrivateRoute path="/admin/manter-problema" roles={[Role.Empresa]} component={ManterProblema} />
+    <PrivateRoute path="/admin/manter-contato" roles={[Role.Empresa]} component={ManterContato} />
+    <PrivateRoute path="/admin/manter-tarefa" roles={[Role.Aluno]} component={ListagemTarefas} />
+    <PrivateRoute path="/admin/calendario" roles={[Role.Aluno]} component={Calendario} />
 
   </Switch>
 );
